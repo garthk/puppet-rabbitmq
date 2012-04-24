fd3ec41a846836553a5f9417899e29555135e848
Garth Kidd <garth@garthk.com>
2012-04-24 16:34:58 +1000

    README update for Issue #1; unsuccessful workaround.

--- tests/vagrant.pp
+++ tests/vagrant.pp
@@ -1,3 +1,26 @@
+stage { pre: before => Stage[main] }
+
+class apt_get_update {
+  $sentinel = "/var/lib/apt/first-puppet-run"
+
+  exec { "initial apt-get update":
+    command => "/usr/bin/apt-get update && touch ${sentinel}",
+    onlyif  => "/usr/bin/env test \\! -f ${sentinel} || /usr/bin/env test \\! -z \"$(find /etc/apt -type f -cnewer ${sentinel})\"",
+    timeout => 3600,
+    notify  => Exec["initial apt-get dist-upgrade"],
+  }
+
+  exec { "initial apt-get dist-upgrade":
+    refreshonly => true,
+    command     => "/usr/bin/apt-get dist-upgrade -y",
+    timeout     => 3600,
+  }
+}
+
+class { 'apt_get_update':
+  stage => pre
+}
+
 group { 'puppet':
   ensure => present,
 }
